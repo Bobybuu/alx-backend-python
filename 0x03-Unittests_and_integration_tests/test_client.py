b92f23af33f1a0193487b9e3e05677fadacd8faa
Bobybuu <c43057772@gmail.com>
2024-08-02 13:14:20 -0700

    test_client.py

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+"""A module for testing the client module.
+"""
+import unittest
+from typing import Dict
+from unittest.mock import (
+    MagicMock,
+    Mock,
+    PropertyMock,
+    patch,
+)
+from parameterized import parameterized, parameterized_class
+from requests import HTTPError
+
+from client import (
+    GithubOrgClient
+)
+from fixtures import TEST_PAYLOAD
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """_summary_
+
+    Args:
+            unittest (_type_): _description_
+    """
+    @parameterized.expand([
+        ("google", {'login': "google"}),
+        ("abc", {'login': "abc"}),
+    ])
+    @patch(
+        "client.get_json",
+    )
+    def test_org(self, org: str, expected_response: Dict,
+                 mocked_function: MagicMock) -> None:
+        """_summary_
+
+        Args:
+                org (str): _description_
+                expected_response (Dict): _description_
+                mocked_function (MagicMock): _description_
+        """
+        mocked_function.return_value = MagicMock(
+            return_value=expected_response)
+        goclient = GithubOrgClient(org)
+        self.assertEqual(goclient.org(), expected_response)
+        mocked_function.assert_called_once_with(
+            "https://api.github.com/orgs/{}".format(org)
+        )
+
+    def test_public_repos_url(self) -> None:
+        """_summary_
+        """
+        with patch(
+            "client.GithubOrgClient.org",
+            new_callable=PropertyMock,
+        ) as mock_org:
+            mock_org.return_value = {
+                'repos_url': "https://api.github.com/users/google/repos",
+            }
+            self.assertEqual(
+                GithubOrgClient("google")._public_repos_url,
+                "https://api.github.com/users/google/repos",
+            )
+
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json: MagicMock) -> None:
+        """_summary_
+
+        Args:
+                mock_get_json (MagicMock): _description_
+        """
+        test_payload = {
+            'repos_url': "https://api.github.com/users/google/repos",
+            'repos': [
+                {
+                    "id": 7697149,
+                    "name": "episodes.dart",
+                    "private": False,
+                    "owner": {
+                        "login": "google",
+                        "id": 1342004,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/google/episodes.dart",
+                    "created_at": "2013-01-19T00:31:37Z",
+                    "updated_at": "2019-09-23T11:53:58Z",
+                    "has_issues": True,
+                    "forks": 22,
+                    "default_branch": "master",
+                },
+                {
+                    "id": 8566972,
+
