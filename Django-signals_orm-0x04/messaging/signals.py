cc95bad1cc7b7cc19ca4ec9a065bfedac5a73350
Bobybuu <c43057772@gmail.com>
2025-08-03 22:27:44 +0300

    Log when a user edits a message and save the old content before the edit

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,12 +1,30 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
 
 @receiver(post_save, sender=Message)
 def create_notification_on_new_message(sender, instance, created, **kwargs):
     if created:
+        print("🚨 Signal triggered: creating notification")
         Notification.objects.create(
             user=instance.receiver,
             message=instance,
             content=f"You have a new message from {instance.sender.username}."
         )
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if instance.id is None:
+        return  # New message, not an update
+
+    try:
+        original = Message.objects.get(id=instance.id)
+    except Message.DoesNotExist:
+        return
+
+    if original.content != instance.content:
+        MessageHistory.objects.create(
+            message=instance,
+            old_content=original.content
+        )
+        instance.edited = True
--- Django-signals_orm-0x04/messaging_app/settings.py
+++ Django-signals_orm-0x04/messaging_app/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'messaging',
+    'messaging.apps.MessagingConfig',
     'django.contrib.staticfiles',
 ]
 
