444bb11bb2bab5693da5f9a05926f97f81c9cd98
Chrispin Odiwuor <c43057772@gmail.com>
2025-09-11 01:22:06 +0300

    Added a fully functional messaging API

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -1,8 +1,11 @@
-
+"""
+URL configuration for messaging_app project.
+"""
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),
+    path('api/', include('chats.urls')),  # Include chats app URLs under /api/
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
